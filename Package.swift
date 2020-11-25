4ab4b775d7bab9023da468f9156a50cee46aff4f
Samuel Goodwin <samuel@roundwallsoftware.com>
2020-11-25 18:56:40 +0100

    fkdahlfajf

--- Package.swift
+++ Package.swift
@@ -16,15 +16,20 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0")
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
+        .package(url: "https://github.com/SwiftPackageRepository/ISO639.swift.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LabelSwitch",
-            dependencies: ["Nimble", "Quick"],
             path: "./LabelSwitch/Classes"
+        ),
+        .testTarget(
+            name: "LabelSwitchTests",
+            dependencies: ["Nimble", "Quick", .product(name: "ISO639", package: "ISO639.swift")],
+            path: "./Tests"
         ),
     ]
 )
---
+++ Tests/SomeTest.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Samuel Goodwin on 11/25/20.
+//
+
+import Foundation
+import Quick
+import Nimble
+import ISO639.swift
+
+public struct SomethingAmazing {
+    let property: String
+}
